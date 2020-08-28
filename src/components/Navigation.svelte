b0fe7e79dfa53f1e946de48560603dbb6774fc61
dmoetech <github@moesibert.de>
2020-08-28 13:09:36 +0200

    navigation mobil anpassung

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -11,8 +11,8 @@
 <!-- <div class="overflow-x-hidden sticky top-0 z-50"> -->
 <div class="relative overflow-hidden">
     <CustomContainer customClass="relative bg-indigo-500 z-50">
-        <div class="border-b border-gray-400 py-2 flex relative z-10 ">
-            <a class="text-gray-300 flex items-center hover:text-white cursor-pointer" href="tel:00491712198934">
+        <div class="border-b border-gray-400 py-2 flex relative z-10 flex-col md:flex-row">
+            <a class="text-gray-300 flex items-center hover:text-white cursor-pointer my-3 md:my-0" href="tel:00491712198934">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-2">
                     <path
                         d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z">
@@ -20,7 +20,7 @@
                 </svg>
                 <span class="block">0171 2198934</span>
             </a>
-            <a class="text-gray-300 flex items-center hover:text-white cursor-pointer ml-auto"
+            <a class="text-gray-300 flex items-center hover:text-white cursor-pointer md:ml-auto mb-3 md:mb-0"
                 href="mailto:kontakt@id-gebaeudemanagement.de?subject=Anfrage">
                 <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-2">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
@@ -29,10 +29,10 @@
                 <span class="block">kontakt@id-gebaeudemanagement.de</span>
             </a>
         </div>
-        <div class="absolute bg-indigo-400 bottom-0 right-0 w-full h-800 transform origin-bottom-right rotate-75 z-0"
+        <div class="absolute bg-indigo-400 bottom-0 right-0 w-full h-800 transform origin-bottom-right rotate-75 z-0 {navbarOpen ? 'bg-indigo-500' : ''}"
             style="--transform-translate-y: 80px">
         </div>
-        <div class="absolute bg-indigo-400 bottom-0 left-0 w-full h-800 transform origin-bottom-left -rotate-75 z-0"
+        <div class="absolute bg-indigo-400 bottom-0 left-0 w-full h-800 transform origin-bottom-left -rotate-75 z-0 {navbarOpen ? 'bg-indigo-500' : ''}"
             style="--transform-translate-y: 80px">
         </div>
         <div class="absolute bg-indigo-500 bottom-0 right-0 w-full h-800 transform origin-bottom-right rotate-45 z-0"
@@ -55,7 +55,7 @@
                         d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
                 </svg> -->
                         <img class="w-auto h-20 mr-3" src="logo.jpg" alt="Firmenlogo">
-                        <span class="font-semibold text-xl tracking-tight">I.D. Gebäudemanagement</span>
+                        <span class="font-semibold text-xl tracking-tight hidden md:block">I.D. Gebäudemanagement</span>
                     </div>
                     <div class="block lg:hidden">
                         <button on:click={toggleNavbar}
@@ -81,7 +81,7 @@
                                 Anfrage
                             </a>
                             <a href="kontakt/" on:click="{() => {navbarOpen = false}}"
-                                class="block mt-4 lg:inline-block lg:mt-0 text-gray-100 text-lg hover:border-gray-200 border border-transparent rounded px-2 lg:px-1 p-1 hover:text-white lg:-mr-1 focus:outline-none">
+                                class="block mt-4 mb-4 lg:mb-0 lg:inline-block lg:mt-0 text-gray-100 text-lg hover:border-gray-200 border border-transparent rounded px-2 lg:px-1 p-1 hover:text-white lg:-mr-1 focus:outline-none">
                                 Kontakt
                             </a>
                         </div>
